9c9fd0a4a8810cf7da064a70cfdd6432acc56823
kashiiitech <kashiiitech@gmail.com>
2025-01-28 13:46:24 +0500

    Docker compose YAML file, services as dns etries, seeding our database

--- BackendApii/Program.cs
+++ BackendApii/Program.cs
@@ -13,17 +13,17 @@ app.MapGet("/", () => "Hello world");
 
 app.MapGet("/podcasts", async () =>
 {
-    using var db = new SqlConnection("Server=tcp:localhost;Initial Catalog=podcasts;Persist Security Info=False;User ID=sa;Password=Admin@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+    using var db = new SqlConnection("Server=tcp:database;Initial Catalog=podcasts;Persist Security Info=False;User ID=sa;Password=Dometrain#123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
 
     // Open the connection
     await db.OpenAsync();
 
     var podcasts = await db.QueryAsync<Podcast>("SELECT * FROM Podcasts");
-    return podcasts.Select(x => x.Title);
+    return podcasts.Select(x => x.Name);
 });
 
 
 app.Run();
 
 
-record class Podcast(Guid Id, string Title);
+record class Podcast(Guid Id, string Name);
