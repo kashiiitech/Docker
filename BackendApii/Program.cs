cc3ce1986487ba3609c6d6f1d3e5827fa2058cb6
kashiiitech <kashiiitech@gmail.com>
2025-01-29 13:05:58 +0500

    resolved docker database seed issue --milestone

--- BackendApii/Program.cs
+++ BackendApii/Program.cs
@@ -3,27 +3,35 @@ using Dapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyOrigin());
+app.UseCors("AllowAll");
 
 app.MapGet("/", () => "Hello world");
 
 app.MapGet("/podcasts", async () =>
 {
-    using var db = new SqlConnection("Server=tcp:database;Initial Catalog=podcasts;Persist Security Info=False;User ID=sa;Password=Dometrain#123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+
+    var db = new SqlConnection("Server=tcp:database;Initial Catalog=podcasts;Persist Security Info=False;User ID=sa;Password=Dometrain#123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
 
     // Open the connection
     await db.OpenAsync();
 
-    var podcasts = await db.QueryAsync<Podcast>("SELECT * FROM Podcasts");
-    return podcasts.Select(x => x.Name);
+    return (await db.QueryAsync<Podcast>("SELECT * FROM Podcasts")).Select(x => x.Title);
 });
 
 
 app.Run();
 
 
-record class Podcast(Guid Id, string Name);
+record class Podcast(Guid Id, string Title);
--- Database/wait-and-run.sh
+++ Database/wait-and-run.sh
@@ -4,7 +4,7 @@
 echo "Waiting for SQL Server to be ready..."
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#123 -Q "SELECT 1" > /dev/null 2>&1
+    /opt/mssql-tools18/bin/sqlcmd -S database -U sa -P Dometrain#123 -C -Q "SELECT 1" > /dev/null 2>&1
     if [ $? -eq 0 ]
     then
         echo "SQL Server is ready."
@@ -16,4 +16,4 @@ do
 done
 
 # Run the SQL script
-/opt/mssql-tools/bin/sqlcmd -S database -U sa -P Dometrain#123 -d master -i /CreateDatabaseAndSeed.sql
+/opt/mssql-tools18/bin/sqlcmd -S database -U sa -P Dometrain#123 -C -d master -i /CreateDatabaseAndSeed.sql
